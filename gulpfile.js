19c7f8126c3ece614781c66b1a0a8dee9eb538c4
Mike Graham <mgraham@technicalmedia.com>
2015-09-22 14:07:31 -0600

    basic website working via gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var serve = require('gulp-serve');
+var browserSync = require('browser-sync').create();
+
+//browserSync.create();
+
+// gulp.task('serve', function(done) {
+//   browserSync.init({
+//     open: false,
+//     port: 9000,
+//     server: {
+//       baseDir: ['.'],
+//       middleware: function (req, res, next) {
+//         res.setHeader('Access-Control-Allow-Origin', '*');
+//         next();
+//       }
+//     }
+//   }, done);
+// });
+
+function reportChange(event){
+  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+}
+
+gulp.task('watch', ['serve'], function() {
+  gulp.watch('./**/*.css', [browserSync.reload]).on('change', reportChange);
+  gulp.watch('./**/*.js', [browserSync.reload]).on('change', reportChange);
+  gulp.watch('.//*.html', [browserSync.reload]).on('change', reportChange);
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
