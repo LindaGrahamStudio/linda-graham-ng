a1053c94f89f7071efa4ab8ed079ced21f151c6a
Mike Graham <mgraham@technicalmedia.com>
2015-10-13 09:51:30 -0600

    video now working dynamically (had to add vimeo to whitelist)

--- app.module.js
+++ app.module.js
@@ -1,3 +1,12 @@
 (function(module) {
-	
+
+	module.config(function($sceDelegateProvider) {
+	  $sceDelegateProvider.resourceUrlWhitelist([
+	    // Allow same origin resource loads.
+	    'self',
+	    // Allow loading from our assets domain.  Notice the difference between * and **.
+	    'http://player.vimeo.com/**'
+	  ]);
+	});
+
 }(angular.module('app', ['ui.router', 'ui.bootstrap'])));
